d62d791d56ae56b581da4251d96d7ba479bd8630
Eda Çam <eda.cam@avkaryazilim.com.tr>
2022-03-02 23:05:56 +0300

    Rent A Car project added.

---
+++ rentACar/src/main/java/com/turkcell/rentACar/api/controllers/BrandsController.java
@@ -0,0 +1,60 @@
+package com.turkcell.rentACar.api.controllers;
+
+import java.util.List;
+
+import javax.validation.Valid;
+
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.turkcell.rentACar.business.abstracts.BrandService;
+import com.turkcell.rentACar.business.dtos.BrandListDto;
+import com.turkcell.rentACar.business.dtos.GetBrandDto;
+import com.turkcell.rentACar.business.requests.CreateBrandRequest;
+import com.turkcell.rentACar.business.requests.UpdateBrandRequest;
+import com.turkcell.rentACar.core.utilities.results.DataResult;
+import com.turkcell.rentACar.core.utilities.results.Result;
+
+@RestController
+@RequestMapping("/api/brands")
+public class BrandsController {
+
+	private BrandService brandService;
+
+	public BrandsController(BrandService brandService) {
+		super();
+		this.brandService = brandService;
+	}
+
+	@GetMapping("/getall")
+	public DataResult<List<BrandListDto>> getAll() {
+		return this.brandService.getAll();
+	}
+
+	@PostMapping("/add")
+	public Result add(@RequestBody @Valid CreateBrandRequest createBrandRequest){
+		return this.brandService.add(createBrandRequest);
+	}
+
+	@GetMapping("/getbyid/{id}")
+	public DataResult<GetBrandDto> getById(@RequestParam int id) {
+		return this.brandService.getById(id);
+	}
+
+	@DeleteMapping("/delete/{id}")
+	public Result delete(@RequestParam int id) {
+		return this.brandService.delete(id);
+	}
+
+	@PutMapping("/update")
+	public Result update(@RequestBody @Valid UpdateBrandRequest updateBrandRequest){
+		return this.brandService.update(updateBrandRequest);
+	}
+
+}
